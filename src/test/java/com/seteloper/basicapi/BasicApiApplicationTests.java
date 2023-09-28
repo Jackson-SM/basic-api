247a323268d9899d022ca5e9f45e021c77ca93a7
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-28 16:48:18 -0300

    init project basic-api

---
+++ src/test/java/com/seteloper/basicapi/BasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seteloper.basicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
