edadb858a7fc4f04c5e49b3060dbdd6330399b73
Jackson Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-09-29 14:38:06 -0300

    feat: add controller with routes

---
+++ src/main/java/com/seteloper/basicapi/controllers/ProductController.java
@@ -0,0 +1,46 @@
+package com.seteloper.basicapi.controllers;
+
+import com.seteloper.basicapi.models.Product;
+import com.seteloper.basicapi.repositories.ProductRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProduct() {
+        return ResponseEntity.ok(productRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        return ResponseEntity.ok(productRepository.save(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(productRepository.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
